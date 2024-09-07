533ae1163d9c039b10982be8ec46a74c9d9a61b8
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-09-08 02:44:11 +0530

    [+]lowpoly cit

--- three/src/App.js
+++ three/src/App.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const App = () => {
   useEffect(() => {
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x87CEEB); 
@@ -18,8 +18,8 @@ const App = () => {
     directionalLight.position.set(5, 5, 5).normalize();
     scene.add(directionalLight);
 
-    camera.position.z = 5;
-
+    camera.position.z = 10;
+    camera.position.y = 20;
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true; 
     controls.dampingFactor = 0.25; 
@@ -34,15 +34,15 @@ const App = () => {
       mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     }
 
-    function onClick(event) {
-      raycaster.setFromCamera(mouse, camera);
+    // function onClick(event) {
+    //   raycaster.setFromCamera(mouse, camera);
 
-      const intersects = raycaster.intersectObjects(scene.children, true);
+    //   const intersects = raycaster.intersectObjects(scene.children, true);
 
-      if (intersects.length > 0) {
-        intersects[0].object.material.color.set(0xff0000);
-      }
-    }
+    //   if (intersects.length > 0) {
+    //     intersects[0].object.material.color.set(0xff0000);
+    //   }
+    // }
 
     function onMiddleClick(event) {
       if (event.button === 1) {
@@ -58,7 +58,7 @@ const App = () => {
     }
 
     window.addEventListener('mousemove', onMouseMove, false);
-    window.addEventListener('click', onClick, false);
+    // window.addEventListener('click', onClick, false);
     window.addEventListener('mousedown', onMiddleClick, false);
 
     function animate() {
@@ -70,7 +70,7 @@ const App = () => {
 
     return () => {
       window.removeEventListener('mousemove', onMouseMove, false);
-      window.removeEventListener('click', onClick, false);
+      // window.removeEventListener('click', onClick, false);
       window.removeEventListener('mousedown', onMiddleClick, false);
       document.body.removeChild(renderer.domElement);
     };
@@ -78,7 +78,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Three.js Model in React, click and goes red</h1>
+      <h1>Three.js Model in React, use click to interact, scroll to zoom in out</h1>
     </div>
   );
 };
--- three/src/loadingmodel.js
+++ three/src/loadingmodel.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 
-loader.load('heart_on_pyramid_stand.glb', function (gltf) {
+loader.load('low_poly_simple_city/scene.gltf', function (gltf) {
     scene.add(gltf.scene);
 }, undefined, function (error) {
     console.error(error);
